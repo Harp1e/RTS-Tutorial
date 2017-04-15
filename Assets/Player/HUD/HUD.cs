aa49ccb851598011665d02f9caa8160fc0a68f09
Harp1e <github@bplayer.waitrose.com>
2017-04-15 14:42:31 +0100

    Pt 9: Player Resources

--- Assets/Player/HUD/HUD.cs
+++ Assets/Player/HUD/HUD.cs
@@ -12,9 +12,14 @@ public class HUD : MonoBehaviour {
     public Texture2D activeCursor;
     public Texture2D selectCursor, leftCursor, rightCursor, upCursor, downCursor;
     public Texture2D[] moveCursors, attackCursors, harvestCursors;
+    public Texture2D[] resources;
+
+    Dictionary<ResourceType, int> resourceValues, resourceLimits;
+    Dictionary<ResourceType, Texture2D> resourceImages;
 
     const int ORDERS_BAR_WIDTH = 150, RESOURCE_BAR_HEIGHT = 40;
     const int SELECTION_NAME_HEIGHT = 15;
+    const int ICON_WIDTH = 32, ICON_HEIGHT = 32, TEXT_WIDTH = 128, TEXT_HEIGHT = 32;
 
     Player player;
     CursorState activeCursorState;
@@ -25,6 +30,27 @@ public class HUD : MonoBehaviour {
         player = transform.root.GetComponent<Player> ();
         ResourceManager.StoreSelectBoxItems (selectBoxSkin);
         SetCursorState (CursorState.Select);
+        resourceValues = new Dictionary<ResourceType, int> ();
+        resourceLimits = new Dictionary<ResourceType, int> ();
+        resourceImages = new Dictionary<ResourceType, Texture2D> ();
+        for (int i = 0; i < resources.Length; i++)
+        {
+            switch (resources[i].name)
+            {
+                case "Money":
+                    resourceImages.Add (ResourceType.Money, resources[i]);
+                    resourceValues.Add (ResourceType.Money, 0);
+                    resourceLimits.Add (ResourceType.Money, 0);
+                    break;
+                case "Power":
+                    resourceImages.Add (ResourceType.Power, resources[i]);
+                    resourceValues.Add (ResourceType.Power, 0);
+                    resourceLimits.Add (ResourceType.Power, 0);
+                    break;
+                default:
+                    break;
+            }
+        }
 	}
 
     void OnGUI ()
@@ -87,11 +113,22 @@ public class HUD : MonoBehaviour {
         }
     }
 
+    public void SetResourceValues (Dictionary<ResourceType, int> resourceValues, Dictionary<ResourceType, int> resourceLimits)
+    {
+        this.resourceValues = resourceValues;
+        this.resourceLimits = resourceLimits;
+    }
+
     private void DrawResourceBar ()
     {
         GUI.skin = resourceSkin;
         GUI.BeginGroup (new Rect (0, 0, Screen.width, RESOURCE_BAR_HEIGHT));
         GUI.Box (new Rect (0, 0, Screen.width, RESOURCE_BAR_HEIGHT), "");
+        int topPos = 4, iconLeft = 4, textLeft = 20;
+        DrawResourceIcon (ResourceType.Money, iconLeft, textLeft, topPos);
+        iconLeft += TEXT_WIDTH;
+        textLeft += TEXT_WIDTH;
+        DrawResourceIcon (ResourceType.Power, iconLeft, textLeft, topPos);
         GUI.EndGroup ();
     }
 
@@ -110,6 +147,14 @@ public class HUD : MonoBehaviour {
             GUI.Label (new Rect (0, 10, ORDERS_BAR_WIDTH, SELECTION_NAME_HEIGHT), selectionName);
         }
         GUI.EndGroup ();
+    }
+
+    void DrawResourceIcon (ResourceType type, int iconLeft, int textLeft, int topPos)
+    {
+        Texture2D icon = resourceImages[type];
+        string text = resourceValues[type].ToString () + "/" + resourceLimits[type].ToString ();
+        GUI.DrawTexture (new Rect (iconLeft, topPos, ICON_WIDTH, ICON_HEIGHT), icon);
+        GUI.Label (new Rect (textLeft, topPos, TEXT_WIDTH, TEXT_HEIGHT), text);
     }
 
     void DrawMouseCursor ()
--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -1,19 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RTS;
 
 public class Player : MonoBehaviour {
 
     public HUD hud;
     public WorldObject SelectedObject { get; set; }
+
     public string username;
     public bool human;
+    public int startMoney, startMoneyLimit, startPower, startPowerLimit;
 
-	void Start () {
+    Dictionary<ResourceType, int> resources, resourceLimits;
+
+    private void Awake ()
+    {
+        resources = InitResourceList ();
+        resourceLimits = InitResourceList ();
+    }
+
+    void Start () {
         hud = GetComponentInChildren<HUD> ();
+        AddStartResourceLimits ();
+        AddStartResources ();
 	}
 	
 	void Update () {
-		
+        if (human)
+        {
+            hud.SetResourceValues (resources, resourceLimits);
+        }
 	}
+
+    private Dictionary<ResourceType, int> InitResourceList ()
+    {
+        Dictionary<ResourceType, int> list = new Dictionary<ResourceType, int> ();
+        list.Add (ResourceType.Money, 0);
+        list.Add (ResourceType.Power, 0);
+        return list;
+    }
+
+    void AddStartResourceLimits ()
+    {
+        IncrementResourceLimit (ResourceType.Money, startMoneyLimit);
+        IncrementResourceLimit (ResourceType.Power, startPowerLimit);
+    }
+
+    void AddStartResources()
+    {
+        AddResource (ResourceType.Money, startMoney);
+        AddResource (ResourceType.Power, startPower);
+    }
+
+    public void AddResource (ResourceType type, int amount)
+    {
+        resources[type] += amount;
+    }
+
+    public void IncrementResourceLimit (ResourceType type, int amount)
+    {
+        resourceLimits[type] += amount;
+    }
 }
